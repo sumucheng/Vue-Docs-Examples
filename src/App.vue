07250b78ed12675bb92d51d93bfd9137e542153f
kiki <772765729@qq.com>
2019-12-02 00:24:44 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <p>{{msg}}</p>
+      <button v-on:click="reverse">反转</button>
     </div>
-    <router-view />
+    <div>
+      <span v-bind:title="title">鼠标悬停几秒钟查看此处动态绑定的提示信息！</span>
+    </div>
+    <div>
+      <p v-if="seen">now you can see me</p>
+    </div>
+    <div>
+      <ol>
+        <li v-for="(todo,index) in todos" :key="index">{{ todo.text }}</li>
+      </ol>
+    </div>
+    <div>
+      <p>{{input}}</p>
+      <input v-model="input" />
+    </div>
+    <div>
+      <ol></ol>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data: () => {
+    return {
+      msg: "Welcome to Vue World",
+      title: "ok",
+      seen: true,
+      input: "1",
+      todos: [{ text: "1" }, { text: "2" }, { text: "3" }]
+    };
+  },
+  methods: {
+    reverse() {
+      this.msg = this.msg
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>

7364d09db672987e48a4d1e4c44066b90d314cce
kiki <772765729@qq.com>
2019-12-02 00:31:48 +0800

    设置正确的publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/Vue-Docs-Examples/" : "/"
+    process.env.NODE_ENV === "production" ? "/Vue-Docs-Examples/dist/" : "/"
 };
